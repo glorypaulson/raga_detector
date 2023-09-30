0f4f61fd54cd879ae86d65499a6c595f0b6fe5d8
glory08 <glorypaulson85@gmail.com>
2023-09-30 13:38:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,16 +3,29 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import UploadFiles from "./components/upload-files.component";
+import backgroundImg from './ragabg.png';
+
 
 function App() {
   return (
+    <div style={{
+      backgroundImage: `url(${backgroundImg})`,
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+      height: '100vh',
+      width: '100vw',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}>
     <div className="container" style={{ width: "600px" }}>
       <div style={{ margin: "20px" }}>
         <h3>Raga Detector</h3>
-        <h4>React upload Files</h4>
+        <h4>Upload Files</h4>
       </div>
-
       <UploadFiles />
+    </div>
     </div>
   );
 }
